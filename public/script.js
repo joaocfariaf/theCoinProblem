d14b48e61406abeda922c77483a905ab0baf2dcd
Arthur Morais <arthurfmmorais@gmail.com>
2020-06-24 23:24:03 -0300

    2 objects on scene

--- public/script.js
+++ public/script.js
@@ -1,5 +1,54 @@
-// GLSL \/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+// Onheto com textura importada
+var cube2 
 
+// Cena
+const scene = new THREE.Scene();
+
+// Iluminação
+const light = new THREE.DirectionalLight('#ffffff', 0.9);
+light.position.set(0, 5, 10);
+scene.add(light);
+
+// Câmera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 14;
+camera.position.x = 0;
+camera.position.y = 5;
+
+// Renderização
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Obtendo textura a partir de uma imagem da web (definindo wrapping)
+var texture = new THREE.TextureLoader().load( "https://media.istockphoto.com/photos/white-color-frosted-glass-texture-background-picture-id696307908?k=6&m=696307908&s=612x612&w=0&h=Bwtr8etrC6DnrQP0pbCP7v14aOO5fw46kCHxvL74CMw=" );
+texture.wrapS = THREE.RepeatWrapping;
+texture.wrapT = THREE.RepeatWrapping;
+texture.repeat.set( 0.5, 2 );
+
+var material = new THREE.MeshLambertMaterial( { color: 0x00ffff, wireframe: false, map:texture } ); 
+
+// // Importando objeto construído no blender
+const objLoader = new THREE.OBJLoader();
+objLoader.setPath('/blender-files/');
+
+const mtlLoader = new THREE.MTLLoader();
+mtlLoader.setPath('/blender-files/');
+
+objLoader.load('cube.obj', (object) => {
+    object.traverse( function( child ) {
+        if ( child instanceof THREE.Mesh ) {
+            child.material = material;
+        }
+    } );
+    cube2 = object;
+
+    scene.add(object);
+    object.position.set(0,9,0)
+});
+
+
+// // GLSL \/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
 const vShader = `
 varying vec3 vNormal;
 
@@ -38,37 +87,8 @@ const uniforms = {
 }
 
 
-var cube2 
-const scene = new THREE.Scene();
-
-const light = new THREE.DirectionalLight('#ffffff', 0.9);
-light.position.set(-20, 0, 100);
-scene.add(light);
-
-// set clock
-const clock = new THREE.Clock();
-
-const camera = new THREE.PerspectiveCamera(75,
-    window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-const objLoader = new THREE.OBJLoader();
-objLoader.setPath('/blender-files/');
-
-const mtlLoader = new THREE.MTLLoader();
-mtlLoader.setPath('/blender-files/');
-
-// // var texture = new THREE.TextureLoader().load( "https://images.pexels.com/photos/235525/pexels-photo-235525.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" );
-// var texture = new THREE.TextureLoader().load( 'https://images.pexels.com/photos/207300/pexels-photo-207300.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' );
-// texture.wrapS = THREE.RepeatWrapping;
-// texture.wrapT = THREE.RepeatWrapping;
-// texture.repeat.set( 0.5, 2 );
-
-var geometry = new THREE.BoxGeometry( 1, 1, 1 ); 
+// Objeto com Shader
+var cube3 
 
 objLoader.load('cube.obj', (object) => {
     object.traverse( function( child ) {
@@ -80,98 +100,27 @@ objLoader.load('cube.obj', (object) => {
                             });;
         }
     } );
-    cube2 = object;
+    cube3 = object;
     scene.add(object);
+    // object.position.set(0,0)
 });
 
-// const material = new THREE.ShaderMaterial({
-//   vertexShader: vShader,
-//   fragmentShader: fShader,
-//   uniforms
-// });
-
-// mesh
-// const cube = new THREE.Mesh(geometry, material);
-// scene.add( cube ); // add to scene
-
+// Adicionando movimento 
 function render() {
   if (cube2) {
-      cube2.rotation.x += 0.01;        
-      cube2.rotation.y += 0.01;
+      cube2.rotation.x += 0.01;
+      cube2.rotation.z += 0.01;
   }
 
-  // update time uniform
-  uniforms.u_time.value = clock.getElapsedTime();
-  // animation loop
-  requestAnimationFrame( render );
-  renderer.render( scene, camera );
+  if (cube3) {
+    cube3.rotation.x += 0.01;
+    cube3.rotation.z += 0.01;
+  }
 
+
+  requestAnimationFrame(render);
+  renderer.render(scene, camera);
 }
 
+
 render();
-
-
-
-
-// // var texture = new THREE.TextureLoader().load( 'https://images.pexels.com/photos/207300/pexels-photo-207300.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' );
-
-
-
-
-
-// // camera
-// let aspect = window.innerWidth / window.innerHeight;
-// const camera = new THREE.PerspectiveCamera( 15, aspect, 1, 1000 );
-// camera.position.z = 10;
-
-// // renderer
-// const renderer = new THREE.WebGLRenderer({
-//   alpha: true,
-//   antialias: true,
-// });
-// renderer.setSize(window.innerWidth, window.innerHeight);
-// document.body.appendChild(renderer.domElement);
-
-
-
-
-
-
-
-
-
-// // EVENT LISTENERS \/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
-// // mousemove
-// document.addEventListener('mousemove', (e) =>{
-//    uniforms.u_mouse.value.x = e.clientX;
-//    uniforms.u_mouse.value.y = e.clientY;
-//  })
-
-
-
-
-// // window resize
-// function onWindowResize( event ) {
-//   const aspectRatio = window.innerWidth/window.innerHeight;
-//   let width, height;
-//   if (aspectRatio>=1){
-//     width = 1;
-//     height = (window.innerHeight/window.innerWidth) * width;
-//   }else{
-//     width = aspectRatio;
-//     height = 1;
-//   }
-//   camera.left = -width;
-//   camera.right = width;
-//   camera.top = height;
-//   camera.bottom = -height;
-//   camera.updateProjectionMatrix();
-//   renderer.setSize( window.innerWidth, window.innerHeight );
-
-//   if (uniforms.u_resolution !== undefined){
-//     uniforms.u_resolution.value.x = window.innerWidth;
-//     uniforms.u_resolution.value.y = window.innerHeight;
-//   }
-// }
-
-
